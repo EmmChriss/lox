f5903fb83e1061d6750fb7aafe03e8563b4772de
EmmChriss <emmchris@protonmail.com>
2021-04-07 16:05:52 +0300

    error handling

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,10 @@
-use thiserror::Error;
-
 pub type Result<T, E=LoxError> = std::result::Result<T, E>;
 
-#[derive(Error, Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum LoxError {
 	#[error("...")]
-	IoError(#[from] std::io::Error)
+	IoError(#[from] std::io::Error),
+	
+	#[error("lexer error: {:?}", .0)]
+	LexerError(#[from] crate::lexer::LexerError)
 }
